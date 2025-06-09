73442eaa4e1e16e8033924da5703a56a32c7a4fe
zhang-zizhe <zizhe.zh@gmail.com>
2025-06-09 06:49:40 +0000

    Deploying to gh-pages from @ zhang-zizhe/zhang-zizhe.github.io@00f1a04ca5bf3295d0ccde2d32f7a4ce90569a66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Publications by categories in reversed chronological order.",
+          description: "Publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
